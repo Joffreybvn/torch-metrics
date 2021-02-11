587a4995730a7ec7b0370d6dd523d2c84129b255
Joffrey Bienvenu <joffreybvn@gmail.com>
2021-02-11 10:19:57 +0100

    gitignore added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="torch_metrics",
+    name="torch_metrics-corrected",
     version="1.1.7",
     description="Metrics for model evaluation in pytorch",
     url="https://github.com/chinokenochkan/torch-metrics",
